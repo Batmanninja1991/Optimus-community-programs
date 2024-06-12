4171c69a9ae5e42f6f2591776bfbf9c106100740
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-06-12 16:42:26 -0700

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
@@ -6,39 +7,22 @@ import About from "./pages/About";
 import Programs from "./pages/Programs";
 import Activities from "./pages/Activities";
 import Contact from "./pages/Contact";
-import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
 const App = () => {
   return (
-    <div>
+    <Router basename="/Optimus-community-programs">
       <Navbar />
       <Routes>
         <Route index element={<Hero />} />
-        <Route
-          path="https://batmanninja1991.github.io/Optimus-community-programs/"
-          element={<Hero />}
-        />
-        <Route
-          index
-          path="https://batmanninja1991.github.io/Optimus-community-programs/about"
-          element={<About />}
-        />
-        <Route
-          path="https://batmanninja1991.github.io/Optimus-community-programs/programs"
-          element={<Programs />}
-        />
-        <Route
-          path="https://batmanninja1991.github.io/Optimus-community-programs/activities"
-          element={<Activities />}
-        />
-        <Route
-          path="https://batmanninja1991.github.io/Optimus-community-programs/contact"
-          element={<Contact />}
-        />
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
